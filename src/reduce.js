a7b4e8837d764f5742429749965589057dd52e5e
Myung-gwan Kim <cartopia95@naver.com>
2020-06-16 11:58:18 +0900

    add - reduce를 사용한 최댓값, 최솟값

--- src/reduce.js
+++ src/reduce.js
@@ -8,6 +8,7 @@
     - 초기값이 있을 경우 -> accumulator: 초기값, currentValue: array[0], currentIndex: 0 (콜백함수 array.length번 호출)
 */
 
+// 반장선거
 const solution = (students) => {
     const arr = students.split(' ');
     const result = {};
@@ -22,14 +23,20 @@ const solution = (students) => {
     // Object.keys(result) -> ['혜원', '유진', '원범']
     const banJang = Object.keys(result).reduce((accumulator, currentValue) => result[accumulator] > result[currentValue] ? accumulator : currentValue);
     console.log(`${banJang}(이)가 총 ${result[banJang]}표로 반장이 되었습니다.`);
-    console.log(result);
-    
 };
 solution('혜원 유진 유진 원범 원범 혜원 혜원 혜원');
 
+// 점수의 평균값 구하기
 const solution2 = (scores) => {
     const scoreArr = scores.split(' ');
     const avg = scoreArr.map(score => parseInt(score)).reduce((a, b) => a + b) / scoreArr.length;
     return Math.floor(avg);
 };
-console.log(solution2('20 30 40'));
+console.log(solution2('20 30 40')); // 30
+
+// 최댓값, 최솟값 구하기
+const arr = [1, 9, 5, 6, 11, 88, 99, 45, 456];
+const max = arr.reduce((acc, value) => acc > value ? acc : value);
+const min = arr.reduce((acc, value) => acc > value ? value : acc);
+console.log(max); // 456
+console.log(min); // 1
