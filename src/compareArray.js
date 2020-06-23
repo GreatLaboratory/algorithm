09f9153728b2e5596c9c2efc89dd5ab1b4933336
Myung-gwan Kim <cartopia95@naver.com>
2020-06-23 17:05:58 +0900

    feat - 배열 비교

---
+++ src/compareArray.js
@@ -0,0 +1,17 @@
+Array.prototype.equals = function (array) {
+    if (!array) return false;
+    if (this.length != array.length) return false;
+    for (let i = 0, l = this.length; i < l; i++) {
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            if (!this[i].equals(array[i])) return false;       
+        } else if (this[i] != array[i]) { 
+            return false;   
+        }           
+    }       
+    return true;
+};
+
+const arr = [4, 5, 6, 7, 8];
+const arr2 = [4, 5, 6, 7, 8, 1];
+console.log(arr === arr2); // false
+console.log(arr.equals(arr2)); // true
